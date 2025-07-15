51476bead50367d1863828547a56052a00adb11f
Artem Podorozhko <artem.podorozhko@outlook.com>
2025-07-15 13:22:53 +0300

    Sources

---
+++ TradeForge/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TradeForge
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ TradeForge/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TradeForge
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
