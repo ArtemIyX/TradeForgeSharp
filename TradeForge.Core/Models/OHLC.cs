e68860b0eb4eeb9975195b83609386a6ff8695aa
Artem Podorozhko <artem.podorozhko@outlook.com>
2025-07-16 13:58:19 +0300

    OHLC Table

---
+++ TradeForge.Core/Models/OHLC.cs
@@ -0,0 +1,11 @@
+﻿namespace TradeForge.Core.Models;
+
+public class OHLC
+{
+    public DateTime Timestamp { get; set; } = DateTime.MinValue;
+    public decimal Open { get; set; } = 0;
+    public decimal High { get; set; } = 0;
+    public decimal Low { get; set; } = 0;
+    public decimal Close { get; set; } = 0;
+    public decimal Volume { get; set; } = 0;
+}
