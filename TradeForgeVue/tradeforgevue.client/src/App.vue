ddb7ad0b414bca93277fecfe0cddb07aa956da6e
Artem Podorozhko <artem.podorozhko@outlook.com>
2026-01-13 13:12:31 +0200

    Basic client setup (router + 3 views)

---
+++ TradeForgeVue/tradeforgevue.client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+        <router-link to="/">Dashboard</router-link>
+        <router-link to="/history">History Manager</router-link>
+        <router-link to="/backtester">Backtester</router-link>
+    </nav>
+    <router-view />
+</template>
+
+<script setup>
+
+</script>
+
+
+
+
+<style scoped>
+
+</style>
