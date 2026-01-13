0d6fe51b69017ada89f46e27bd29c020f5654f1d
Artem Podorozhko <artem.podorozhko@outlook.com>
2026-01-13 15:56:33 +0200

    Symbol editor: seperate component

---
+++ TradeForgeVue/tradeforgevue.client/src/components/SymbolEditor.vue
@@ -0,0 +1,139 @@
+﻿<template>
+  <v-container>
+    <v-form ref="form" v-model="valid">
+      <v-row>
+        <!-- left: generic fields -->
+        <v-col cols="12" md="6">
+          <v-text-field
+            v-model="local.ticker"
+            label="Ticker *"
+            :rules="[rules.required]"
+          />
+
+          <v-select
+            v-model="local.type"
+            label="Type *"
+            :items="typeItems"
+            :rules="[rules.required]"
+          />
+
+          <v-select
+            v-model="local.category"
+            label="Category *"
+            :items="categoryItems"
+            :rules="[rules.required]"
+          />
+
+          <v-textarea
+            v-model="local.description"
+            label="Description"
+            rows="3"
+            auto-grow
+            placeholder="Some description of your asset"
+          />
+        </v-col>
+
+        <!-- right: contract specs -->
+        <v-col cols="12" md="6">
+          <v-text-field
+            v-model.number="local.contractSize"
+            label="Contract size *"
+            type="number"
+            step="1"
+            :rules="[rules.required, rules.positive]"
+          />
+
+          <v-text-field
+            v-model="local.units"
+            label="Units *"
+            :rules="[rules.required]"
+          />
+
+          <v-text-field
+            v-model.number="local.volumeStep"
+            label="Volume step *"
+            type="number"
+            step="0.01"
+            :rules="[rules.required, rules.positive]"
+          />
+
+          <v-text-field
+            v-model.number="local.defaultLeverage"
+            label="Default leverage *"
+            type="number"
+            step="0.01"
+            :rules="[rules.required, rules.positive]"
+          />
+
+          <v-text-field
+            v-model.number="local.minVolume"
+            label="Min volume *"
+            type="number"
+            step="0.01"
+            :rules="[rules.required, rules.positive]"
+          />
+
+          <v-text-field
+            v-model.number="local.maxVolume"
+            label="Max volume *"
+            type="number"
+            step="0.01"
+            :rules="[rules.required, gtMinRule]"
+          />
+
+          <v-text-field
+            v-model.number="local.minTick"
+            label="Min tick *"
+            type="number"
+            step="0.00001"
+            :rules="[rules.required, rules.positive]"
+          />
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+import { required, positive } from '@/utils/rules'
+
+const TYPE_ITEMS = Object.freeze(['CFD', 'Spot', 'Future', 'Option', 'ETF'])
+const CATEGORY_ITEMS = Object.freeze(['Stock', 'Forex', 'Crypto', 'Equity', 'Index', 'Commodity'])
+
+export default {
+  name: 'SymbolEditor',
+  props: {
+    value: { type: Object, required: true }
+  },
+  data() {
+    return {
+      valid: false,
+      local: { ...this.value },
+      typeItems: TYPE_ITEMS,
+      categoryItems: CATEGORY_ITEMS,
+      rules: { required, positive }
+    }
+  },
+  computed: {
+    gtMinRule() {
+      const min = Number(this.local.minVolume)
+      return v => Number(v) >= min || 'Must be ≥ Min volume'
+    }
+  },
+  watch: {
+    value: {
+      handler(newVal) {
+        this.local = { ...newVal }
+      },
+      deep: true
+    },
+    local: {
+      handler(newVal) {
+        this.$emit('input', newVal)
+      },
+      deep: true
+    }
+  }
+}
+</script>
+
--- TradeForgeVue/tradeforgevue.client/src/views/history/HistoryCreate.vue
+++ TradeForgeVue/tradeforgevue.client/src/views/history/HistoryCreate.vue
@@ -1,110 +1,27 @@
 ﻿<template>
+
   <v-container>
-    <v-form ref="form" v-model="valid" @submit.prevent="submit">
-      <v-row>
-        <!-- left: generic fields -->
-        <v-col cols="12" md="6">
-          <v-text-field
-            v-model="symbol.ticker"
-            label="Ticker *"
-            :rules="[required]"
-          />
+    <h1>Create new history item</h1>
+    <SymbolEditor :value="symbol"/>
 
-          <v-select
-            v-model="symbol.type"
-            label="Type *"
-            :items="typeItems"
-            :rules="[required]"
-          />
+    <v-row>
+      <v-col class="d-flex justify-end">
+        <v-btn color="primary" type="submit" :disabled="!valid">Save</v-btn>
+        <v-btn class="ml-2" @click="$router.back()">Cancel</v-btn>
+      </v-col>
+    </v-row>
 
-          <v-select
-            v-model="symbol.category"
-            label="Category *"
-            :items="categoryItems"
-            :rules="[required]"
-          />
-
-          <v-textarea
-            v-model="symbol.description"
-            label="Description"
-            rows="3"
-            placeholder="Some description of your asset"
-          />
-        </v-col>
-
-        <!-- right: contract specs -->
-        <v-col cols="12" md="6">
-          <v-text-field
-            v-model.number="symbol.contractSize"
-            label="Contract size *"
-            type="number"
-            step="1"
-            :rules="[required, positive]"
-          />
-
-          <v-text-field
-            v-model="symbol.units"
-            label="Units *"
-            :rules="[required]"
-          />
-
-          <v-text-field
-            v-model.number="symbol.volumeStep"
-            label="Volume step *"
-            type="number"
-            step="0.01"
-            :rules="[required, positive]"
-          />
-
-          <v-text-field
-            v-model.number="symbol.defaultLeverage"
-            label="Default leverage *"
-            type="number"
-            step="0.01"
-            :rules="[required, positive]"
-          />
-
-          <v-text-field
-            v-model.number="symbol.minVolume"
-            label="Min volume *"
-            type="number"
-            step="0.01"
-            :rules="[required, positive]"
-          />
-
-          <v-text-field
-            v-model.number="symbol.maxVolume"
-            label="Max volume *"
-            type="number"
-            :rules="[required, gtMin]"
-          />
-
-          <v-text-field
-            v-model.number="symbol.minTick"
-            label="Min tick *"
-            type="number"
-            step="0.00001"
-            :rules="[required, positive]"
-          />
-        </v-col>
-      </v-row>
-
-      <v-row>
-        <v-col class="d-flex justify-end">
-          <v-btn color="primary" type="submit" :disabled="!valid">Save</v-btn>
-          <v-btn class="ml-2" @click="$router.back()">Cancel</v-btn>
-        </v-col>
-      </v-row>
-    </v-form>
   </v-container>
 </template>
 
 <script>
 
 
+import SymbolEditor from "@/components/SymbolEditor.vue";
+
 export default {
   name: "CreateSymbol",
-  components: {},
+  components: {SymbolEditor},
   data: () => ({
     valid: false,
     symbol: {
@@ -122,16 +39,10 @@ export default {
       minTick: 0.00001
     },
 
-    typeItems: ['CFD', 'Spot', 'Future', 'Option', 'ETF'],
-    categoryItems: ['Stock', 'Forex', 'Crypto', 'Equity', 'Index', 'Commodity']
+
   }),
 
   methods: {
-    required: v => !!v || 'Required',
-    positive: v => v > 0 || 'Must be positive',
-    gtMin(v) {
-      return !this.symbol || v > this.symbol.minVolume || 'Must be > min volume'
-    },
 
     async submit() {
       if (!this.$refs.form.validate()) return
