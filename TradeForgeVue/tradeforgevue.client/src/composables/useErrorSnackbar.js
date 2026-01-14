227fb290a67b047c845cae50fd8433557974cea3
Artem Podorozhko <artem.podorozhko@outlook.com>
2026-01-14 22:24:05 +0200

    eror snackbar js

---
+++ TradeForgeVue/tradeforgevue.client/src/composables/useErrorSnackbar.js
@@ -0,0 +1,15 @@
+﻿import { ref } from 'vue'
+
+const errors = ref([])
+
+export function useErrorSnackbar() {
+  const showGlobalError = (text, color = 'error') => {
+    errors.value.push({ text, color, id: Date.now() })
+  }
+
+
+  return {
+    errors,
+    showGlobalError,
+  }
+}
