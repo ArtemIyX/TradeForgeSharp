1a08d7508183723f510ff16b76847d368dcf2614
Artem Podorozhko <artem.podorozhko@outlook.com>
2026-01-29 18:10:06 +0200

    Back button

---
+++ TradeForgeVue/tradeforgevue.client/src/components/BackButton.vue
@@ -0,0 +1,20 @@
+﻿<template>
+  <v-btn
+    icon="mdi-arrow-left"
+    variant="text"
+    size="small"
+    class="mr-2"
+    @click="goBack"
+  />
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goBack = () => {
+  router.go(-1)
+}
+
+</script>
