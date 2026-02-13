61c518bee713c92b9a58e91747bb6a42fa5f7e0d
Artem Podorozhko <artem.podorozhko@outlook.com>
2026-02-13 12:57:13 +0200

    basic setup

--- TradeForge.VUE/src/router/index.ts
+++ TradeForge.VUE/src/router/index.ts
@@ -2,7 +2,23 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/views/home/HomeView.vue')
+    },
+    {
+      path: '/data-manager',
+      name: 'data-manager',
+      component: () => import('@/views/data-manager/DataManagerView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: () => import('@/views/not-found/NotFoundView.vue')
+    }
+  ],
 })
 
 export default router
---
+++ TradeForge.VUE/src/views/not-found/NotFoundView.vue
@@ -0,0 +1,18 @@
+﻿<template>
+
+
+</template>
+
+<script setup lang="ts">
+import {useRouter} from 'vue-router'
+
+const router = useRouter()
+
+const goHome = () => {
+  router.push({name: 'home'})
+}
+</script>
+
+<style scoped>
+
+</style>
