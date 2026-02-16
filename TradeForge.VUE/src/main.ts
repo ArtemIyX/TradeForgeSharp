97f4b3d496a06c876d2ce2629e85798e0e0daefd
Artem Podorozhko <artem.podorozhko@outlook.com>
2026-02-16 15:07:57 +0200

    ECharts setup

--- TradeForge.VUE/src/main.ts
+++ TradeForge.VUE/src/main.ts
@@ -1,10 +1,41 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import { createVuetify } from 'vuetify'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
+import {createVuetify} from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
+
+import ECharts from 'vue-echarts'
+import {use} from 'echarts/core'
+
+// Import required ECharts components
+import {CanvasRenderer} from 'echarts/renderers'
+import {CandlestickChart, LineChart, BarChart} from 'echarts/charts'
+import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  GridComponent,
+  DataZoomComponent,
+  MarkLineComponent,
+  MarkPointComponent
+} from 'echarts/components'
+
+// Register components
+use([
+  CanvasRenderer,
+  CandlestickChart,
+  LineChart,
+  BarChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  GridComponent,
+  DataZoomComponent,
+  MarkLineComponent,
+  MarkPointComponent
+])
 
 import App from './App.vue'
 import router from './router'
@@ -16,6 +47,7 @@ const vuetify = createVuetify({
   directives,
 })
 
+app.component('v-chart', ECharts)
 app.use(vuetify)
 app.use(createPinia())
 app.use(router)
