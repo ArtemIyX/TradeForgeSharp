5fd8bd9ced405bdf3a5a731a5db246503c4e0cec
Artem Podorozhko <artem.podorozhko@outlook.com>
2026-02-18 14:57:51 +0200

    Closed Trade item interface

---
+++ TradeForge.VUE/src/types/strategy/StrategyTradeItem.interface.ts
@@ -0,0 +1,23 @@
+﻿export interface StrategyTradeItem {
+  magic: number; // strategy magic number
+  ticket: number; // trade id to find in chart
+  symbol: string; // symbol name
+
+  openTime: Date;
+  openPrice: number;
+
+  size: number; // lots
+
+  closeTime: Date;
+  closePrice: number;
+
+  profitOrLoss: number;
+  balanceAfter: number;
+  closeType: string; // SL, TP, Trailling Stop, End of friday etc
+
+  mae: number;
+  mfe: number;
+  timeInTrade: string;
+
+  comment: string;
+}
