6d0b97f7cdf00308bffe8ec5bd2a48276d00702c
Artem Podorozhko <artem.podorozhko@outlook.com>
2026-02-21 10:12:54 +0200

    Program.cs setup

--- TradeForge.Backend/Program.cs
+++ TradeForge.Backend/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseWebSockets(); // Enable WebSocket middleware
+app.MapControllers();
 
 app.Run();
